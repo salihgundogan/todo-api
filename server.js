aa3957a301c52aa94cd8a84a31a575408d0d7009
salihgundogan <salih.gundogan@istun.edu.tr>
2025-08-04 10:09:26 +0300

    son hali

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ const path = require('path');
 const fastify = require('fastify')({ logger: true });
 
 fastify.register(require('@fastify/cors'), {
-  origin: 'http://localhost:8080', 
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
+    origin: 'http://localhost:8080', 
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
 });
-
-const fastifyStatic = require('@fastify/static');
-
-
 fastify.register(require('@fastify/multipart'));
-
 fastify.register(require('@fastify/static'), {
     root: path.join(__dirname, 'uploads'),
     prefix: '/uploads/',
@@ -20,28 +15,21 @@ fastify.register(require('@fastify/static'), {
 const categoryRoutes = require('./src/routes/category.routes');
 const todoRoutes = require('./src/routes/todo.routes');
 
-
 fastify.register(categoryRoutes, { prefix: '/api' });
 fastify.register(todoRoutes, { prefix: '/api' });
 
 fastify.get('/', async (request, reply) => {
-    return { hello: 'world' };
+    return { status: 'ok', message: 'API is running' };
 });
-
-
-
 
 const start = async () => {
     try {
         const port = process.env.PORT || 3000;
         await fastify.listen({ port: port, host: '0.0.0.0' });
-        fastify.log.info(`Sunucu ${fastify.server.address().port} portunda calisiyor`);
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
     }
 };
-
-console.log('Trello API Key:', process.env.TRELLO_API_KEY);
 
 start();
